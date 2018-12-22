339e88f1dfc6a154ac3805c722f462f0ea873dba
AllanChain <txsmlf@gmail.com>
2018-12-22 21:59:58 +0800

    update FTPDownloader

--- FTPDownloader.py
+++ FTPDownloader.py
@@ -1,6 +1,8 @@
 from ftplib import FTP
 from sys import stdout
 import time
+from os import _exit,stat,utime,popen
+from os.path import isfile
 
 
 class FileTracker():
@@ -19,7 +21,7 @@ class FileTracker():
         self.file.write(buff)
     def  __del__(self):
         self.file.close()
-ftp = FTP()
+
 def init(host,port,user,password):
     ftp.connect(host,port)
     ftp.login(user,password)
@@ -27,14 +29,28 @@ def init(host,port,user,password):
 
 def download(directory,filename,dest):
     ftp.cwd(directory)
+    ftp_mtime=compare_mtime(filename,dest)
+    local_file=FileTracker(dest,ftp.size(filename))
+    ftp.retrbinary('RETR %s'%filename,local_file.write)
+    del local_file
+    utime(dest,(ftp_mtime,ftp_mtime))
+    print(stat(dest))
+    #ftp.dir()
+
+def compare_mtime(filename,dest):
     L=ftp.sendcmd('MDTM %s'%filename)
     dir_t=L[4:8]+'-'+L[8:10]+'-'+L[10:12]+' '+L[12:14]+':'+L[14:16]+':'+L[16:18]
     timeArray = time.strptime(dir_t, "%Y-%m-%d %H:%M:%S")
-    timeStamp = int(time.mktime(timeArray))
-    print(timeStamp)
-    local_file=FileTracker(dest,ftp.size(filename))
-    ftp.retrbinary('RETR %s'%filename,local_file.write)
-    #ftp.dir()
+    ftp_mtime = int(time.mktime(timeArray))
+    if isfile(dest):
+        local_file=stat(dest)
+        print(ftp_mtime,local_file)
+        if ftp_mtime == local_file.st_mtime:
+            popen('"%s"'%dest)
+            _exit(0)
+        if ftp_mtime >= local_file.st_mtime:
+            popen('DEL "%s"'%dest)
+    return ftp_mtime
 def format_size(size):
     for i in ('B','K','M','G'):
         if size>900:
@@ -42,7 +58,11 @@ def format_size(size):
         else:
             return '{:.1f}'.format(size)+i
 ##download('新文件夹','新建 文本文档.txt')
-init('192.168.123.99',2121,'ac','123')
-download('documents','申请表.pdf','123.pdf')
-input()
-##ftp.dir()
+
+ftp = FTP()
+if __name__ == '__main__':
+    init('192.168.123.99',2121,'ac','123')
+    download('documents','申请表.pdf','123.pdf')
+    input()
+    ##ftp.dir()
+
