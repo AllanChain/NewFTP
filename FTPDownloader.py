642ad44cf6cd5fa84bcd5124e88dbf4b706fc38e
AllanChain <txsmlf@gmail.com>
2018-12-23 08:56:37 +0800

    update FTPDownloader

--- FTPDownloader.py
+++ FTPDownloader.py
@@ -1,7 +1,7 @@
 from ftplib import FTP
 from sys import stdout
 import time
-from os import _exit,stat,utime,popen
+from os import _exit,stat,utime,popen,system
 from os.path import isfile
 
 
@@ -10,6 +10,7 @@ class FileTracker():
         self.file=open(filename,'wb')
         self.filesize=filesize
         self.current_size=0
+        
     def write(self,buff):
         self.current_size+=len(buff)
         ratio=self.current_size/self.filesize
@@ -19,43 +20,56 @@ class FileTracker():
         stdout.write(' |'.join((percentage,size,bar))+'\r')
         stdout.flush()
         self.file.write(buff)
-    def  __del__(self):
+    def  close(self):
         self.file.close()
 
 def init(host,port,user,password):
+    global USER,PASSWORD
+    USER,PASSWORD=user,password
     ftp.connect(host,port)
     ftp.login(user,password)
     ftp.encoding='utf-8'
 
-def download(directory,filename,dest):
-    ftp.cwd(directory)
-    ftp_mtime=compare_mtime(filename,dest)
-    ftp_filesize=ftp.size(filename)
-    local_file=FileTracker(dest,ftp_filesize)
-    print('File to download:',directory+'/'+filename)
-    print('Remote file size:',ftp_filesize)
-    print('Remote file modify time:',ftp_mtime)
-    print('Local file name:',dest)
+def just_download(directory,filename,dest,ftp_mtime,ftp_filesize):
+    #ftp_mtime,ftp_filesize=compare_mtime(directory,filename,dest)
+    
+    if PRINTING == True:
+        print('File to download:',directory+'/'+filename)
+        print('Remote file size:',format_size(ftp_filesize))
+        print('Remote file modify time:',ftp_mtime)
+        print('Local file name:',dest)
+        print()
+        local_file=FileTracker(dest,ftp_filesize)
+    else:
+        local_file=open(dest,'wb')
+##    ftp.dir()
     ftp.retrbinary('RETR %s'%filename,local_file.write)
-    del local_file
+    local_file.close()
     utime(dest,(ftp_mtime,ftp_mtime))
-    print(stat(dest))
+    popen('"%s"'%dest)
+    #print(stat(dest))
     #ftp.dir()
 
-def compare_mtime(filename,dest):
+def download(directory,filename,dest):
+    ftp.cwd(directory)
     L=ftp.sendcmd('MDTM %s'%filename)
     dir_t=L[4:8]+'-'+L[8:10]+'-'+L[10:12]+' '+L[12:14]+':'+L[14:16]+':'+L[16:18]
     timeArray = time.strptime(dir_t, "%Y-%m-%d %H:%M:%S")
     ftp_mtime = int(time.mktime(timeArray))
     if isfile(dest):
         local_file=stat(dest)
-        print(ftp_mtime,local_file)
         if ftp_mtime == local_file.st_mtime:
             popen('"%s"'%dest)
             _exit(0)
         if ftp_mtime >= local_file.st_mtime:
             popen('DEL "%s"'%dest)
-    return ftp_mtime
+    ftp_filesize=ftp.size(filename)
+    if ftp_filesize < SILENT:
+        just_download(directory,filename,dest,ftp_mtime,ftp_filesize)
+    else:
+        cmd='python FTPDownloader.py %s %s %s %s %s %d %d'\
+             %(USER,PASSWORD,directory,filename,dest,ftp_mtime,ftp_filesize)
+        system(cmd)
 def format_size(size):
     for i in ('B','K','M','G'):
         if size>900:
@@ -65,9 +79,23 @@ def format_size(size):
 ##download('新文件夹','新建 文本文档.txt')
 
 ftp = FTP()
+USER=''
+PASSWORD=''
+SILENT=1024*800
 if __name__ == '__main__':
-    init('192.168.123.99',2121,'ac','123')
-    download('documents','申请表.pdf','123.pdf')
-    input()
-    ##ftp.dir()
+    from sys import argv
 
+    user,password,directory,filename,dest,ftp_mtime,ftp_filesize=argv[1:8]
+    PRINTING=True
+    system('mode 70,10')
+    system('color f2')
+    system('title A simple downloader')
+    print('This is a simple FTP downloader')
+    print()
+    init('192.168.123.99',2121,user,password)
+    ftp.cwd(directory)
+    just_download(directory,filename,dest,int(ftp_mtime),int(ftp_filesize))
+    time.sleep(0.5)
+else:
+    PRINTING=False
+    ##ftp.dir()
