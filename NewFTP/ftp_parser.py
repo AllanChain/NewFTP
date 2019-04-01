fe291171c1b67eb27ac54ecb2a733f03df98a845
Allan Chain <txsmlf@gmail.com>
2019-04-01 17:09:35 +0800

    iconvert to python filee config

--- NewFTP/ftp_parser.py
+++ NewFTP/ftp_parser.py
@@ -1,16 +1,8 @@
-import yaml
-
-
 def get_host_port(server):
     if ':' in server:
         return server.split(':')
     else:
         return server, 21
 
-
-with open('ftp_server.yaml', 'r', encoding='utf-8') as f:
-    consts = yaml.load(f)
-SERVER = COMPLETE_SERVER = consts['server']
-ENCODING = consts['encoding']
-DEFAULT_PASS = str(consts['default_password'])
-SILENT = consts['silent']
+with open('config.py', encoding='utf-8') as config:
+    exec(config.read())
