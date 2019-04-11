b801262159b74a3e623a147ff63b737d2d7fd4fe
Allan Chain <txsmlf@gmail.com>
2019-04-11 17:11:33 +0800

    add python config file to data/

---
+++ NewFTP/data/config.py
@@ -0,0 +1,30 @@
+# add special rules here
+specials = {'.*/(.*mp4)': 'mess',
+            'zjx/303/(.*)': '哈哈哈',
+            'zjp/(.*)': '地理',
+            'xmh/(.*mp3)': '英语\听力',
+            'xmh/(.*xls)': '英语\统计',
+            'xmh/(.*)': '英语\课件',
+            'ysh/(.*)': '地理',
+            'czw/(.*)': '英语',
+            'sgf/(.*)': '技术',
+            'zzx/(.*)': ''}
+LOCAL_PREFIX = r'D:\Desktop\\'
+USERS = {'语文': 'zm',
+         '数学': 'cjun',
+         '英语': 'xmh',
+         '陈忠伟': 'czw',
+         '化学': 'zjx',
+         '钟志兴': 'zzx',
+         '团委': 'tgtw',
+         '物理': 'zxs',
+         '地理杨': 'ysh',
+         '地理周': 'zjp',
+         '沈国飞': 'sgf',
+        }
+PASSWORDS = {'zzx': '1234'}
+STYLE = 'MaterialBlue'
+SERVER = COMPLEATE_SERVER =  '6.163.193.243:21'
+ENCODING = 'gbk'
+DEFAULT_PASS = '123'
+SILENT = 2 * 1024 **2
